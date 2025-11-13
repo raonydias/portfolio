f3942ea7e409e1c79a3d07d15848e1e07b995941
raonydias <rodriguesraony@gmail.com>
2025-11-13 19:45:52 -0300

    Uso de Gist em API fake

--- projects/alura/memoteca/js/api.js
+++ projects/alura/memoteca/js/api.js
@@ -3,7 +3,9 @@ const URL_BASE = "http://localhost:3000";
 const api = {
     async buscarPensamentos() {
         try {
-            const response = await axios.get(`${URL_BASE}/pensamentos`);
+            const response = await axios.get(
+                "https://gist.githubusercontent.com/raonydias/d39e67b1e1c60a16d2fd6d8459c02894/raw/901957b71d49b06095dc9f8d51cd8ab1a4717410/pensamentos.txt"
+            );
             return await response.data;
         } catch (error) {
             alert("Erro ao buscar pensamentos");
