1ed95cfbcba1b01aa1c256ee802916a2aa8825d0
raonydias <rodriguesraony@gmail.com>
2025-11-13 19:31:03 -0300

    04. Usando uma biblioteca externa

--- projects/alura/vidflow/script.js
+++ projects/alura/vidflow/script.js
@@ -1,9 +1,11 @@
+import axios from "axios";
+
 const containerVideos = document.querySelector(".videos__container");
 
 async function buscarEMostrarVideos() {
     try {
-        const busca = await fetch("http://localhost:3000/videos");
-        const videos = await busca.json();
+        const busca = await axios.get("http://localhost:3000/videos");
+        const videos = await busca.data;
 
         videos.forEach((video) => {
             if (video.categoria == "") {
