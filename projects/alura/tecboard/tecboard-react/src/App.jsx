f7561d3acfe0d9c21a6b25cfe959825ec054aedc
raonydias <rodriguesraony@gmail.com>
2025-11-14 14:38:21 -0300

    03. Trabalhando com listas

--- projects/alura/tecboard/tecboard-react/src/App.jsx
+++ projects/alura/tecboard/tecboard-react/src/App.jsx
@@ -1,16 +1,61 @@
 import "./App.css";
+import { Banner } from "./componentes/Banner";
 import { FormularioDeEvento } from "./componentes/FormularioDeEvento";
+import { Tema } from "./componentes/Tema";
+import { CardEvento } from "./componentes/CardEvento";
 
 function App() {
+    const temas = [
+        {
+            id: 1,
+            nome: "front-end",
+        },
+        {
+            id: 2,
+            nome: "back-end",
+        },
+        {
+            id: 3,
+            nome: "devops",
+        },
+        {
+            id: 4,
+            nome: "inteligência artificial",
+        },
+        {
+            id: 5,
+            nome: "data science",
+        },
+        {
+            id: 6,
+            nome: "cloud",
+        },
+    ];
+
+    const eventos = [
+        {
+            capa: "https://raw.githubusercontent.com/viniciosneves/tecboard-assets/refs/heads/main/imagem_1.png",
+            tema: temas[0],
+            data: new Date(),
+            titulo: "Mulheres no Front",
+        },
+    ];
+
     return (
         <main>
             <header>
                 <img src="/logo.png" alt="Logo da Tecboard" />
             </header>
-            <section>
-                <img src="/banner.png" alt="Banner de tecnologia" />
-            </section>
+            <Banner />
             <FormularioDeEvento />
+            {temas.map(function (item) {
+                return (
+                    <section key={item.id}>
+                        <Tema tema={item}></Tema>
+                        <CardEvento evento={eventos[0]}></CardEvento>
+                    </section>
+                );
+            })}
         </main>
     );
 }
