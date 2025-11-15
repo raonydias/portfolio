0f77e7cf962ff997aaf1a35d1fab85df27a7a8bd
raonydias <rodriguesraony@gmail.com>
2025-11-15 11:18:16 -0300

    02. Condicionais e concatenação

--- studies/alura/languages/JavaScript/logica-js/app.js
+++ studies/alura/languages/JavaScript/logica-js/app.js
@@ -1,7 +1,11 @@
 //alert("Boas vindas ao jogo do número secreto.");
 let numeroSecreto = 5;
+console.log(numeroSecreto);
 let chute = prompt("Escolha um número entre 1 e 10");
 
+// Se o chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    console.log("Isso aí! Você descobriu o número secreto (5)");
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    alert("Você errou :(");
 }
