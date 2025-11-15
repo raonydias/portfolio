397a6dad745390d18edec5004579fe2376cfafbc
raonydias <rodriguesraony@gmail.com>
2025-11-15 11:29:15 -0300

    03. Loops e tentativas

--- studies/alura/languages/JavaScript/logica-js/app.js
+++ studies/alura/languages/JavaScript/logica-js/app.js
@@ -1,11 +1,19 @@
 //alert("Boas vindas ao jogo do número secreto.");
 let numeroSecreto = 5;
-console.log(numeroSecreto);
-let chute = prompt("Escolha um número entre 1 e 10");
+let chute = "";
+let tentativas = 0;
 
-// Se o chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert("Você errou :(");
+// Enquanto chute não for igual ao número secreto
+while (chute != numeroSecreto) {
+    tentativas++;
+    chute = prompt("Escolha um número entre 1 e 10");
+
+    // Se o chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+    } else if (chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`O número secreto é maior que ${chute}`);
+    }
 }
