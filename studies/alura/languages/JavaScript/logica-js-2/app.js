aa0cd7f4a47b0ed6e2d097357db6c74bcbffeb8c
raonydias <rodriguesraony@gmail.com>
2025-11-15 12:00:49 -0300

    01. Interagindo com HTML

---
+++ studies/alura/languages/JavaScript/logica-js-2/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector("h1");
+titulo.textContent = "Número Secreto";
+
+let paragrafo = document.querySelector("p");
+paragrafo.textContent = "Escolha um número entre 1 e 10:";
+
+function verificarChute() {
+    console.log("Botão clicado");
+}
--- studies/alura/languages/JavaScript/logica-js/app.js
+++ studies/alura/languages/JavaScript/logica-js/app.js
@@ -1,5 +1,5 @@
 //alert("Boas vindas ao jogo do número secreto.");
-let numeroMaximo = 5000;
+let numeroMaximo = 100;
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 let chute = "";
 let tentativas = 0;
